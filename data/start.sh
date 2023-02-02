2273a71fed84162c76a0d0e2ad7d5a3b078ece52
damienmillet <contact@damien-millet.dev>
2023-02-02 20:51:26 +0100

    Update start.sh

--- data/start.sh
+++ data/start.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-echo $SSH_KEY > ~/.ssh/authorized_keys
-jq --null-input --arg path "$GIT_PATH" '{"git.path": $path,}' > ~/.vscode-server/data/Machine/settings.json
-rc-service sshd start
-echo "Starting VSCode Server"
-echo "You can now connect to this container"
-echo "if a token wasn't given, you can must add the generated token to your github account"
-
-/bin/sh
+/usr/sbin/sshd -D -e $@
